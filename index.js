2d4432f49259a6b2db51e6a7025f417706a8559f
jessmchang <jessmchang@gmail.com>
2014-04-06 01:48:40 -0700

    h1, p work

---
+++ index.js
@@ -0,0 +1,32 @@
+var express = require("express");
+var app = express();
+var port = 3700;
+
+
+app.set('views', __dirname + '/tpl');
+app.set('view engine', "jade");
+app.engine('jade', require('jade').__express);
+app.get("/", function(req, res){
+    res.render("page");
+});
+
+app.use(express.static(__dirname + '/public'));
+
+var io = require('socket.io').listen(app.listen(port));
+
+io.sockets.on('connection', function (socket) {
+	var client = socket.id;
+    socket.emit('message', { message: 'welcome to the chat' });
+    socket.on('send', function (data) {
+        io.sockets.emit('message', data);
+        console.log("success");
+    });
+
+    socket.on('div-change', function (data) {
+    	io.sockets.emit('div-change', data);
+    	console.log("data" + data);
+    });
+});
+
+
+console.log("Listening on port " + port);
---
+++ public/chat.js
@@ -0,0 +1,135 @@
+var fonts;
+var h1Elements;
+var socket;
+var messages;
+var field;
+var sendButton;
+var content;
+var header;
+
+
+// function getRandomFont() {
+//         var randomNumber = Math.floor(Math.random()*(fonts.length));
+//         // console.log(fonts[randomNumber]);
+//         return fonts[randomNumber];
+//     }
+
+
+function change(a, type) {
+        // var randomFont = getRandomFont();
+        var currentFont = a.innerHTML;
+        // console.log("type " + type);
+        if(type=="header"){
+            // console.log("current " + currentFont);
+            for(h in h1Elements) {
+                if(h1Elements[h] instanceof Element) {
+                    h1Elements[h].style.fontFamily=currentFont;
+                }
+            }
+        }
+        if(type=="para"){
+            // console.log("para only");
+            // console.log("pElements " + pElements[0]);
+            for(p in pElements){
+                if(pElements[p] instanceof Element) {
+                    console.log(p);
+                    pElements[p].style.fontFamily=currentFont;
+                }
+            }
+        }
+        
+        
+        socket.emit('div-change', { currentFont: currentFont, type: type });
+    }
+
+/*
+*generates all fonts
+*header
+*p
+*/
+function generateFonts(){
+        for(font in fonts){
+            var a = document.createElement('a');
+            var aText = document.createTextNode(fonts[font]);
+            a.appendChild(aText);
+            a.href = "#";
+            a.setAttribute('onclick', 'change(this, "header");');
+            header.appendChild(a);
+        }
+        for(font in fonts){
+            var b = document.createElement('a');
+            var bText = document.createTextNode(fonts[font]);
+            b.appendChild(bText);
+            b.href = "#";
+            b.setAttribute('onclick', 'change(this, "para");');
+            para.appendChild(b);
+        }
+    }
+ 
+
+
+window.onload = function() {
+ 
+    messages = [];
+    socket = io.connect('10.24.25.63:3700');
+    field = document.getElementById("field");
+    sendButton = document.getElementById("send");
+    content = document.getElementById("content");
+    fonts=['Montserrat', 'Georgia', "Times New Roman"];
+    h1Elements=document.querySelectorAll('h1');
+    pElements=document.querySelectorAll('p');
+    header=document.getElementById("header");
+    header2=document.getElementById("header2");
+    para=document.getElementById("para");
+
+    generateFonts();
+   
+ 
+    socket.on('message', function (data) {
+        if(data.message) {
+            messages.push(data.message);
+            var html = '';
+            for(var i=0; i<messages.length; i++) {
+                html += messages[i] + '<br />';
+            }
+            content.innerHTML = html;
+            content.scrollTop = content.scrollHeight;
+        } else {
+            console.log("There is a problem:", data);
+        }
+    });
+
+    socket.on('div-change', function (data) {
+        // console.log(data.currentFont);
+        console.log(data.type);
+        if(data.type=="header"){
+             for(h in h1Elements) {
+                if(h1Elements[h] instanceof Element) {
+                    h1Elements[h].style.fontFamily=data.currentFont;
+                }
+            }
+        }
+        if(data.type=="para"){
+            for(p in pElements) {
+                if(pElements[p] instanceof Element) {
+                    pElements[p].style.fontFamily=data.currentFont;
+                }
+            }
+        }
+           
+    });
+
+
+    //returns a random font
+    sendButton.onclick = function() {
+        console.log(sendButton);
+        var text = field.value;
+        socket.emit('send', { message: text });
+        field.value="";
+    };
+
+
+
+    
+        //header font has to change
+}
